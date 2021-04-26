666bc22903cccc3426fbaa2556d1142506323dd9
b1wang <64937203+b1wang@users.noreply.github.com>
2021-04-25 17:03:33 -0700

    Update part1b-question18.js

--- part1/part1b-question18.js
+++ part1/part1b-question18.js
@@ -1,1 +1,13 @@
+var interval;
 
+function question18() {
+	interval = setInterval(printTime, 1000);
+}
+
+function printTime() {
+    let d = new Date();
+	  let time = d.toLocaleTimeString();
+    console.log(time);     
+}
+
+question18();
